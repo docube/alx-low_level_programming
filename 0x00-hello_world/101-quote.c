f6594feaf8e1dafed148708a1a5dcc772d703402
docube <segunolawepo@rocketmail.com>
2022-09-08 21:43:29 -0700

    commite

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
-#include <unistd.h>
+#include<unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+ * main - main - prints the standrad error sandwiched around the  standard output
+ * 
+ * Return: returns 1
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\";
-
-	write(2, str1, 59);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
