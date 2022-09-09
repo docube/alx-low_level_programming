d0f971c53d2dc9d40d2236eea16c2ed926b93d0c
docube <segunolawepo@rocketmail.com>
2022-09-08 19:02:41 -0700

    Omo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,10 @@
  * Return: returns 1
  */
 int main(void)
-{	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\";
+
+	write(2, str1, 59);
+
 	return (1);
 }
